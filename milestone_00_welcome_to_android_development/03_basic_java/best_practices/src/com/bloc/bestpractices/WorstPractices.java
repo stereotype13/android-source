5fe9c3437918605a9cfc0385ce333d6f30a0e72b
Robert Hodel <stereotype13@hotmail.com>
2014-06-03 19:29:30 -0700

    Corrected the Java worst practices

--- milestone_00_welcome_to_android_development/03_basic_java/best_practices/src/com/bloc/bestpractices/WorstPractices.java
+++ milestone_00_welcome_to_android_development/03_basic_java/best_practices/src/com/bloc/bestpractices/WorstPractices.java
@@ -9,30 +9,43 @@ public class WorstPractices extends Object {
 // EDIT BELOW
 
 	public static void main(String [] args) {
-	int MAGIC_NUMBER = WorstPractices.animals(false);
-	MAGIC_NUMBER *= 5;
-	if (MAGIC_NUMBER > 18) {
-	while(MAGIC_NUMBER > 0)
-	{
-	MAGIC_NUMBER--;
-	}
-	}
+
+		int magicNumber = WorstPractices.animals(false);
+
+		magicNumber *= 5;
+
+		if (magicNumber > 18) {
+
+			while(magicNumber > 0) {
+				magicNumber--;
+			}
+		}
 	}
 
-// animals
-// this method takes in a single parameter, yeswellheresthething. Using a very elaborate and complex algorithm, it calculate a magic number
-// yeswellheresthething: a seed which helps generate the magic number
-// returns: a magical number
-private static int animals(boolean yeswellheresthething)
-{
-		/*
-			Start off with one of these
-		*/
-	int anint = yeswellheresthething ? 34 : 21;
-	float sparklesfairy = .5f;
-	for (int brown = 0; brown < aInt; brown++) { sparklesfairy *= aInt;
-	} return (int) sparklesfairy * aInt;
-}
+	/* animals
+	 this method takes in a single parameter, seed. Using a very elaborate and complex algorithm, it calculate a magic number
+	 seed: a seed which helps generate the magic number
+	 returns: a magical number 
+	 */
+
+	private static int animals(boolean seed)
+	{
+			
+		//Start off with one of these
+			
+		int number = seed ? 34 : 21;
+
+		float sparklesFairy = 0.5f;
+
+		for (int brown = 0; brown < number; ++brown) { 
+
+			sparklesFairy *= number;
+			
+		} 
+
+		return (int) sparklesFairy * number;
+
+	}
 
 // STOP EDITING
 }
