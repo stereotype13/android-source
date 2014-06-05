99bc08eab9edee9f9667b924e887dde45bb88e4a
Robert Hodel <stereotype13@hotmail.com>
2014-06-05 10:08:54 -0700

    Completed Java Assignments Checkpoint

--- milestone_00_welcome_to_android_development/03_basic_java/operations/src/com/bloc/ops/Operations.java
+++ milestone_00_welcome_to_android_development/03_basic_java/operations/src/com/bloc/ops/Operations.java
@@ -14,32 +14,45 @@ public class Operations extends Object {
 			ASSIGNMENT:
 			Place 18 into x
 		*/
+		x = 18;
+
 		int y = 0;
 		/*
 			ASSIGNMENT:
 			Pre-increment x and assign it to y in a single statement
 		*/
+		y = ++x;
+
 		int z = 0;
 		/*
 			ASSIGNMENT:
 			Multiply x by y, assign it to z
 		*/
+		z = x * y;
+
 		int remainder = 0;
 		/*
 			ASSIGNMENT:
 			Mod z by 17 and assign the result to remainder
 		*/
+		remainder = z % 17;
+
 		float floaty = 0f;
 		/*
 			ASSIGNMENT:
 			Assign 5 to floaty and then divide it by 3
 		*/
+		floaty = 5f;
+		floaty = floaty / 3;
+
 		double dubs = 0d;
 		/*
 			ASSIGNMENT:
 			Assign 5.3 into dubs
 			Then post-Decrement dubs and multiply it by itself in a single statement
 		*/
+		dubs = 5.3d;
+		dubs = dubs-- * dubs;
 
 		if (!Test.testX(x)) {
 			System.out.print("Sorry, looks like something's wrong with 'x'\n");
