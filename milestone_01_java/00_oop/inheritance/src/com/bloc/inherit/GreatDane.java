de0c5399d14d4f80e322105b05da375405e40ab1
Robert Hodel <stereotype13@hotmail.com>
2014-06-08 18:34:08 -0700

    Completed the INHERITANCE checkpoint

--- milestone_01_java/00_oop/inheritance/src/com/bloc/inherit/GreatDane.java
+++ milestone_01_java/00_oop/inheritance/src/com/bloc/inherit/GreatDane.java
@@ -1,3 +1,37 @@
 package com.bloc.inherit;
 
-// CLASS DEFINITION GOES HERE
+class GreatDane extends Dog {
+  @Override
+  int getSizeIndex(String size) {   
+
+    if(size == "huge") {
+      return 4;
+    }
+    else {
+      return super.getSizeIndex(size);
+    }
+  }
+
+  @Override
+  String fromSizeIndex(int index) {
+
+    if(index == 4) {
+      return "huge";
+    }
+    else {
+      return super.fromSizeIndex(index);
+    }
+  }
+
+  @Override
+  void changeSize(boolean grow) {
+    int sizeIndex = getSizeIndex();
+    sizeIndex = sizeIndex + (grow ? 1 : -1);
+    if (sizeIndex > 4) {
+      sizeIndex = 4;
+    } else if (sizeIndex < 0) {
+      sizeIndex = 0;
+    }
+    setSize(fromSizeIndex(sizeIndex));
+  }
+}
