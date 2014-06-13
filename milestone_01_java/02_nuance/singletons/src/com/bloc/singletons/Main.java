822e6ee3c8adfdde38bd189ddcae49d42536bfbb
Joe Smith <stereotype13@hotmail.com>
2014-06-13 15:21:06 -0700

    Working on SINGLETONS. One error at compele time.

--- milestone_01_java/02_nuance/singletons/src/com/bloc/singletons/Main.java
+++ milestone_01_java/02_nuance/singletons/src/com/bloc/singletons/Main.java
@@ -1,10 +1,31 @@
 package com.bloc.singletons;
 
+import com.bloc.singletons.listeners.*;
+import com.bloc.singletons.talkers.*;
+
 public class Main extends Object {
 
 	public static void main(String [] args) {
 		// Instantiate some talkers and some listeners
 		// Register listeners
 		// Send messages!
+		
+		AudienceMember am = new AudienceMember();
+		Child child = new Child();
+		Pet pet = new Pet();
+
+		Parent parent = new Parent();
+		PetOwner petOwner = new PetOwner();
+		Singer singer = new Singer();
+
+		Speakerphone mSpeakerPhone = Speakerphone.get();
+		mSpeakerPhone.addListener(am);
+		mSpeakerPhone.addListener(child);
+		mSpeakerPhone.addListener(pet);
+
+		mSpeakerPhone.shoutMessage(parent);
+		mSpeakerPhone.shoutMessage(petOwner, pet.getClass());
+
+		return;
 	}
 }
--- milestone_01_java/02_nuance/singletons/src/com/bloc/singletons/Speakerphone.java
+++ milestone_01_java/02_nuance/singletons/src/com/bloc/singletons/Speakerphone.java
@@ -1,32 +1,63 @@
 package com.bloc.singletons;
 
+import java.util.*;
+
 /*
  * This is a singleton class which maintains communication
  * between classes and Listeners
  */
 public class Speakerphone extends Object {
+	
+
+	private static Speakerphone mSpeakerphone;
+	private List<Listener> mListeners; 
+
+	private Speakerphone() {
+		mListeners = new ArrayList<Listener>();
+	}
+
 	/*
 	 * get
 	 * @return the singleton instance of Speakerphone
 	 */
+	public static Speakerphone get() {
+		if(mSpeakerphone == null) {
+			mSpeakerphone = new Speakerphone();
+		}
+		return mSpeakerphone;
+	}
 
 	/*
 	 * addListener
 	 * Add a listener to Speakerphone's list
 	 * @param listener an instance of the Listener interface
 	 */
+	public void addListener(Listener listener) {
+		mListeners.add(listener);
+		return;
+	}
 
 	/*
 	 * removeListener
 	 * Remove a Listener from the Speakerphone's list
 	 * @param listener the Listener to remove
 	 */
+	public void removeListener(Listener listener) {
+		mListeners.remove(listener);
+		return;
+	}
 
 	/*
 	 * shoutMessage
 	 * Sends the message to all of the Listeners tracked by Speakerphone
 	 * @param talker a Talker whose message will be sent
 	 */
+	public void shoutMessage(Talker talker) {
+		for(Listener listener: mListeners) {
+			listener.onMessageReceived(talker.getMessage());
+		}
+		return;
+	}
 
 	/*
 	 * shoutMessage
@@ -39,10 +70,23 @@ public class Speakerphone extends Object {
 	 * HINT: see Class.isAssignableFrom()
 	 *		 http://docs.oracle.com/javase/7/docs/api/java/lang/Class.html#isAssignableFrom(java.lang.Class)
 	 */
+	public void shoutMessage(Talker talker, java.lang.Class cls) {
+		for(Listener listener: mListeners) {
+			if(listener.class.isAssignableFrom(cls)) {
+				listener.onMessageReceived(talker.getMessage());
+			}
+			
+		}
+		return;
+	}
 
 	/*
 	 * removeAll
 	 * Removes all Listeners from Speakerphone
 	 */
+	public void removeAll() {
+		mListeners = new ArrayList<Listener>(0);
+		return;
+	}
 
 }
