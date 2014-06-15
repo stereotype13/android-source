acf1b577a18173a4887084890872bb4c6d35e736
Robert Hodel <stereotype13@hotmail.com>
2014-06-15 00:17:15 -0700

    Completed THREADS checkpoint

--- milestone_01_java/02_nuance/threads/src/com/bloc/threads/ImageGetter.java
+++ milestone_01_java/02_nuance/threads/src/com/bloc/threads/ImageGetter.java
@@ -1,8 +1,66 @@
 package com.bloc.threads;
 
+import java.net.URL;
+import java.io.*;
+import javax.imageio.*;
+import java.awt.image.BufferedImage;
+
 public class ImageGetter extends Thread {
+
+  private String mURL;
+  private boolean mLaunchFile;
+  private boolean mExists;
+
+  public ImageGetter() {
+    mLaunchFile = true;
+    mURL = "https://www.google.com/images/srpr/logo11w.png";
+    mExists = false;
+  }
+
+  public ImageGetter(boolean launchFile, String customURL) {
+    mLaunchFile = launchFile;
+    mURL = customURL;
+    mExists = false;
+  }
+
+  public boolean fileExists() {
+    return mExists;
+  }
+
 	@Override
 	public void run() {
-		// TODO
+		try {
+      File existingImage = new File("google_logo.png");
+      if (existingImage.exists()) {
+        existingImage.delete();
+      }
+      URL url = new URL(mURL);
+      BufferedImage bufferedImage = ImageIO.read(url);
+      File outputfile = new File("google_logo.png");
+      ImageIO.write(bufferedImage, "png", outputfile);
+      if(mLaunchFile) {
+        if ("/".equals(System.getProperties().getProperty("file.separator"))) {
+          Runtime.getRuntime().exec("open google_logo.png");
+        } else {
+          Runtime.getRuntime().exec("google_logo.png");
+        }
+      }
+      
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.exit(1);
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+
+    File logo = new File("google_logo.png");
+    mExists = false;
+    try {
+      mExists = logo.exists();
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
 	}
 }
--- milestone_01_java/02_nuance/threads/src/com/bloc/threads/Main.java
+++ milestone_01_java/02_nuance/threads/src/com/bloc/threads/Main.java
@@ -1,9 +1,6 @@
 package com.bloc.threads;
 
-import java.net.URL;
-import java.io.*;
-import javax.imageio.*;
-import java.awt.image.BufferedImage;
+
 
 public class Main extends Object {
 
@@ -11,41 +8,15 @@ public class Main extends Object {
 
 		// Extract content beneath to ImageGetter
 
-		try {
-			File existingImage = new File("google_logo.png");
-			if (existingImage.exists()) {
-				existingImage.delete();
-			}
-			URL url = new URL("https://www.google.com/images/srpr/logo11w.png");
-			BufferedImage bufferedImage = ImageIO.read(url);
-			File outputfile = new File("google_logo.png");
-			ImageIO.write(bufferedImage, "png", outputfile);
-			if ("/".equals(System.getProperties().getProperty("file.separator"))) {
-				Runtime.getRuntime().exec("open google_logo.png");
-			} else {
-				Runtime.getRuntime().exec("google_logo.png");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
+		ImageGetter imageGetter = new ImageGetter();
 
-		File logo = new File("google_logo.png");
-		boolean exists = false;
-		try {
-			exists = logo.exists();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
+		imageGetter.run();
+
 
 		// ^^^ Extract the above to ImageGetter
 
 		// This shouldn't exist yet, therefore we should be able to print 
-		if (exists == false) {
+		if (imageGetter.fileExists() == false) {
 			System.out.println("/************************/");
 			System.out.println("/*                      */");
 			System.out.println("/*                      */");
