4aa364fb309a60bc2c9279c82fe00e7cc8166135
Joe Smith <stereotype13@hotmail.com>
2014-06-17 11:08:44 -0700

    Added editText to fragment

--- blocnotes/blocnotes/BlocNotes/src/main/java/com/bloc/blocnotes/NoteFragment.java
+++ blocnotes/blocnotes/BlocNotes/src/main/java/com/bloc/blocnotes/NoteFragment.java
@@ -1,6 +1,12 @@
 package com.bloc.blocnotes;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 /**
  * Created by stereotype13 on 6/15/14.
@@ -9,5 +15,19 @@ public class NoteFragment extends Fragment {
     NoteFragment() {
 
     }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        EditText editText = new EditText(getActivity());
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        //linearLayout.addView(editView, params);
+        //container.addView(editText, params);
+        editText.setLayoutParams(params);
+        return editText;
+    }
+
+
 }
 
